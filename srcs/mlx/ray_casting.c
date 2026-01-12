0259339f53dc8d445f1c972e03097b5a8c1c7c26
Maati Caruana <macaruan@f1br5s3.clusters.42paris.fr>
2026-01-12 15:22:12 +0100

    cub finished?

--- srcs/mlx/ray_casting.c
+++ srcs/mlx/ray_casting.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ray_casting.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: awaegaer <awaegaer@student.42.fr>          +#+  +:+       +#+        */
+/*   By: macaruan <macaruan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 15:54:09 by awaegaer          #+#    #+#             */
-/*   Updated: 2025/12/19 11:52:46 by awaegaer         ###   ########.fr       */
+/*   Updated: 2026/01/12 14:19:22 by macaruan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,11 +29,11 @@ int	digital_differential_analysis_ops(t_player *player, t_rc_ctx *rc_ctx)
 			* rc_ctx->delta_dist_x;
 	}
 	if (rc_ctx->ray_dir_y < 0)
-		return (rc_ctx->step_y = -1, rc_ctx->side_dist_y
-			= (player->pos_y - rc_ctx->map_y) * rc_ctx->delta_dist_y, 42);
+		return (rc_ctx->step_y = -1, rc_ctx->side_dist_y = (player->pos_y
+				- rc_ctx->map_y) * rc_ctx->delta_dist_y, 42);
 	else
-		return (rc_ctx->step_y = 1, rc_ctx->side_dist_y
-			= (rc_ctx->map_y + 1.0 - player->pos_y) * rc_ctx->delta_dist_y, 42);
+		return (rc_ctx->step_y = 1, rc_ctx->side_dist_y = (rc_ctx->map_y + 1.0
+				- player->pos_y) * rc_ctx->delta_dist_y, 42);
 }
 
 void	project_ray_until_wall(t_game *game, t_rc_ctx *rc_ctx)
@@ -57,6 +57,32 @@ void	project_ray_until_wall(t_game *game, t_rc_ctx *rc_ctx)
 	}
 }
 
+// void	project_ray_until_wall(t_game *game, t_rc_ctx *rc_ctx)
+// {
+// 	digital_differential_analysis_ops(&game->player, rc_ctx);
+// 	while (1)
+// 	{
+// 		if (rc_ctx->map_y < 0 || rc_ctx->map_y >= game->map.height
+// 			|| rc_ctx->map_x < 0 || rc_ctx->map_x >= game->map.width)
+// 			break ;
+// 		if (game->map.grid[rc_ctx->map_y][rc_ctx->map_x] == '1')
+// 			break ;
+// 		if (rc_ctx->side_dist_x < rc_ctx->side_dist_y)
+// 		{
+// 			rc_ctx->map_x += rc_ctx->step_x;
+// 			rc_ctx->side_dist_x += rc_ctx->delta_dist_x;
+// 			rc_ctx->side = 0;
+// 		}
+// 		else
+// 		{
+// 			rc_ctx->map_y += rc_ctx->step_y;
+// 			rc_ctx->side_dist_y += rc_ctx->delta_dist_y;
+// 			rc_ctx->side = 1;
+// 		}
+// 		//digital_differential_analysis_ops(&game->player, rc_ctx);
+// 	}
+// }
+
 void	ray_casting(t_game *game)
 {
 	t_rc_ctx	rc_ctx;
@@ -68,10 +94,10 @@ void	ray_casting(t_game *game)
 	while (rc_ctx.x < WINDOW_WIDTH)
 	{
 		rc_ctx.camera_x = 2 * ((double)rc_ctx.x / (double)WINDOW_WIDTH) - 1;
-		rc_ctx.ray_dir_x = game->player.dir_x
-			+ game->player.cam_x * rc_ctx.camera_x;
-		rc_ctx.ray_dir_y = game->player.dir_y
-			+ game->player.cam_y * rc_ctx.camera_x;
+		rc_ctx.ray_dir_x = game->player.dir_x + game->player.cam_x
+			* rc_ctx.camera_x;
+		rc_ctx.ray_dir_y = game->player.dir_y + game->player.cam_y
+			* rc_ctx.camera_x;
 		rc_ctx.map_x = (int)game->player.pos_x;
 		rc_ctx.map_y = (int)game->player.pos_y;
 		project_ray_until_wall(game, &rc_ctx);
--- srcs/mlx/render.c
+++ srcs/mlx/render.c
@@ -6,7 +6,7 @@
 /*   By: macaruan <macaruan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 15:54:48 by awaegaer          #+#    #+#             */
-/*   Updated: 2026/01/09 16:29:04 by macaruan         ###   ########.fr       */
+/*   Updated: 2026/01/12 14:21:59 by macaruan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	put_pixel(t_img *img, int x, int y, int color)
 {
 	int	offset;
 
+	if (x < 0 || x >= WINDOW_WIDTH || y < 0 || y >= WINDOW_HEIGHT)
+		return ;
 	offset = (y * img->line_length) + (x * (img->bits_per_pixel / 8));
 	*(unsigned int *)(img->addr + offset) = color;
 }
@@ -56,7 +58,7 @@ int	render_frame(void *game_ptr)
 {
 	t_game	*game;
 
-	game = (t_game *) game_ptr;
+	game = (t_game *)game_ptr;
 	update_pos_n_dir(game);
 	render_floor_n_ceiling(game);
 	ray_casting(game);
